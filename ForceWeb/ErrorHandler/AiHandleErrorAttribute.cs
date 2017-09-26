d203837466aa6c3f21ac82f95fa69d2ba78d7006
André Geuze <andre.geuze@avanade.com>
2017-09-26 21:29:02 +0200

    handle error attribute fix

--- ForceWeb/ErrorHandler/AiHandleErrorAttribute.cs
+++ ForceWeb/ErrorHandler/AiHandleErrorAttribute.cs
@@ -1,4 +1,22 @@
-using System;using System.Web.Mvc;using Microsoft.ApplicationInsights;namespace ForceWeb.ErrorHandler{    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]     public class AiHandleErrorAttribute : HandleErrorAttribute    {        public override void OnException(ExceptionContext filterContext)        {            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)            {                var ai = new TelemetryClient();
+using System;
+using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace ForceWeb.ErrorHandler
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)] 
+    public class AiHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
+                var ai = new TelemetryClient();
                 ai.InstrumentationKey = TelemetryConfiguration.Active.InstrumentationKey;
                 ai.TrackException(filterContext.Exception);
-            }            base.OnException(filterContext);        }    }}
+            }
+            base.OnException(filterContext);
+        }
+    }
+}
