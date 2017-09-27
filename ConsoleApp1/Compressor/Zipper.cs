4254c370ce9698b13405b96e23cfd87c0f929b9f
Erwin van Drongelen <erwin.van.drongelen@avanade.com>
2017-09-27 17:54:08 +0200

    consoleapp

---
+++ ConsoleApp1/Compressor/Zipper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace Compressor
+{
+    public class Zipper
+    {
+        public void CompressDing()
+        {
+            ZipConstants.DefaultCodePage = System.Text.Encoding.Default.CodePage;
+
+            FileStream fsOut = File.Create(@"c:\Users\erwin.van.drongelen\Desktop\test\test.zip");
+            ZipOutputStream zipStream = new ZipOutputStream(fsOut);
+
+            zipStream.SetLevel(3); //0-9, 9 being the highest level of compression
+            const string folderName = @"c:\Users\erwin.van.drongelen\Desktop\test\test2";
+
+            var folderOffset = folderName.Length + (folderName.EndsWith("\\") ? 0 : 1);
+
+            CompressFolder(folderName, zipStream, folderOffset);
+
+            zipStream.IsStreamOwner = true; // Makes the Close also Close the underlying stream
+            zipStream.Close();
+        }
+
+        private void CompressFolder(string path, ZipOutputStream zipStream, int folderOffset)
+        {
+
+            var files = Directory.GetFiles(path);
+
+            foreach (var filename in files)
+            {
+
+                var fi = new FileInfo(filename);
+
+                var entryName = filename.Substring(folderOffset); // Makes the name in zip based on the folder
+                entryName = ZipEntry.CleanName(entryName); // Removes drive from name and fixes slash direction
+                var newEntry = new ZipEntry(entryName)
+                {
+                    DateTime = fi.LastWriteTime,
+                    Size = fi.Length
+                };
+
+                zipStream.PutNextEntry(newEntry);
+
+                // Zip the file in buffered chunks
+                // the "using" will close the stream even if an exception occurs
+                var buffer = new byte[4096];
+                using (var streamReader = File.OpenRead(filename))
+                {
+                    StreamUtils.Copy(streamReader, zipStream, buffer);
+                }
+                zipStream.CloseEntry();
+            }
+            var folders = Directory.GetDirectories(path);
+            foreach (var folder in folders)
+            {
+                CompressFolder(folder, zipStream, folderOffset);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Compressor;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static readonly Zipper Zipper = new Zipper();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Zipper.CompressDing();
+        }
+    }
+}
